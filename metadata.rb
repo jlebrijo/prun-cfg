f2c0f7f41c0d6f83257d62dc33e7b6c2a54b254a
Juan Lebrijo <juan@lebrijo.com>
2014-11-06 13:23:07 +0100

    Change recipe name to prun-cfg

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name "prun-stack"
+name "prun-cfg"
 version "0.0.1"
 supports 'ubuntu'
 depends 'ssh_known_hosts'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,8 +2,8 @@ execute "apt-get update" do
   command "apt-get update"
 end
 
-include_recipe "prun-stack::ssh_keys"
-include_recipe "prun-stack::postgresql"
-include_recipe "prun-stack::rails_app"
-include_recipe "prun-stack::nginx"
+include_recipe "prun-cfg::ssh_keys"
+include_recipe "prun-cfg::postgresql"
+include_recipe "prun-cfg::rails_app"
+include_recipe "prun-cfg::nginx"
 
