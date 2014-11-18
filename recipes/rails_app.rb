7a5087a430c0163476143af4ba9edfb6c58d8131
Juan Lebrijo <juan@lebrijo.com>
2014-11-18 12:39:49 +0100

    Removing not needed file directories

--- recipes/rails_app.rb
+++ recipes/rails_app.rb
@@ -31,23 +31,13 @@ node["apps"].each_with_index do |app, i|
 
 end
 
-# Images directories
-directory "/var/www/www/shared/public/uploads/" do
-  recursive true
-end
-directory "/var/www/app/shared/public/" do
-  recursive true
-end
-link "/var/www/app/shared/public/uploads" do
-  to "/var/www/www/shared/public/uploads"
-end
-
 # Common code repo
 if node.has_key? "common_repo"
   git "/var/www/common" do
     repository node["common_repo"]
   end
   node["apps"].each do |app|
+    directory "/var/www/#{app}/releases/"
     link "/var/www/#{app}/releases/common" do
       to "/var/www/common"
     end
