4fa65e1867e2e2fb00e2086127ff6fe7a9c36e9d
Juan Lebrijo <juan@lebrijo.com>
2015-10-14 20:40:12 +0200

    postgres by default for rails apps

--- metadata.rb
+++ metadata.rb
@@ -3,6 +3,6 @@ description "Helps to configure PRUN servers (PostgreSQL / Ruby on Rails/ Ubuntu
 license 'MIT'
 maintainer 'Juan Lebrijo'
 maintainer_email 'juan@lebrijo.com'
-version "0.2.8"
+version "0.2.9"
 supports 'ubuntu'
 depends 'ssh_known_hosts'
